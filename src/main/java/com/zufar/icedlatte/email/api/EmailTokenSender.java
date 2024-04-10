edd7c047363a55a18502558549062cfc9132fce6
Nick Niki <nnick44@mail.ru>
2024-04-10 21:56:21 +0300

    IL- endpoint get verification email code for test: POST /api/v1/auth/email/code

--- src/main/java/com/zufar/icedlatte/email/api/EmailTokenSender.java
+++ src/main/java/com/zufar/icedlatte/email/api/EmailTokenSender.java
@@ -1,9 +1,11 @@
 package com.zufar.icedlatte.email.api;
 
+import com.zufar.icedlatte.email.api.token.TokenCache;
 import com.zufar.icedlatte.email.api.token.TokenManager;
 import com.zufar.icedlatte.email.sender.AuthTokenEmailConfirmation;
 import com.zufar.icedlatte.security.dto.UserRegistrationRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +14,22 @@ public class EmailTokenSender {
 
     private final AuthTokenEmailConfirmation emailConfirmation;
     private final TokenManager tokenManager;
+    private final TokenCache tokenCache;
+
+    @Value("${temporary-cache.get-token-enabled}")
+    private boolean getTokenEnabled = false;
 
     public void sendEmailVerificationCode(final UserRegistrationRequest request) {
         String token = tokenManager.generateToken(request);
         emailConfirmation.sendTemporaryCode(request.email(), token);
+    }
+
+    // for test purposes only
+    public String getEmailVerificationCode(final UserRegistrationRequest request) {
+        if (getTokenEnabled) {
+            return tokenCache.getToken(request);
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/com/zufar/icedlatte/email/api/token/TokenCache.java
+++ src/main/java/com/zufar/icedlatte/email/api/token/TokenCache.java
@@ -4,6 +4,7 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.zufar.icedlatte.email.exception.IncorrectTokenException;
 import com.zufar.icedlatte.security.dto.UserRegistrationRequest;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +13,11 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class TokenCache {
 
-    private Integer EXPIRE_TIME;
-
     private final Cache<String, UserRegistrationRequest> tokenCache;
 
     public TokenCache(@Value("${temporary-cache.time.token}") Integer expireTime) {
-        this.EXPIRE_TIME = expireTime;
         this.tokenCache = CacheBuilder.newBuilder()
-                .expireAfterWrite(EXPIRE_TIME, TimeUnit.MINUTES)
+                .expireAfterWrite(expireTime, TimeUnit.MINUTES)
                 .build();
     }
 
@@ -37,5 +35,19 @@ public class TokenCache {
 
     public void removeToken(String tokenKey) {
         tokenCache.invalidate(tokenKey);
+    }
+
+    // for test only
+    public String getToken(UserRegistrationRequest request) {
+        return getKeyByValue(tokenCache, request);
+    }
+
+    private String getKeyByValue(Cache<String, UserRegistrationRequest> tokenCache, UserRegistrationRequest request) {
+        for (Map.Entry<String, UserRegistrationRequest> entry : tokenCache.asMap().entrySet()) {
+            if (request.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 }
