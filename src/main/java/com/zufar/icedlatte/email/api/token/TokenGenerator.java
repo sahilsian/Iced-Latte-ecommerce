74dd59e87d77b85d4c9e12e45ca6ef521e7890d1
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-04-30 15:14:41 +0100

    Token pattern was updated

--- src/main/java/com/zufar/icedlatte/email/api/token/TokenGenerator.java
+++ src/main/java/com/zufar/icedlatte/email/api/token/TokenGenerator.java
@@ -9,7 +9,7 @@ import java.security.SecureRandom;
 public class TokenGenerator {
     private static final SecureRandom random = new SecureRandom();
     private static final String BASE = "0123456789";
-    private static final String PATTERN = "###-###-###";
+    private static final String PATTERN = "#########";
     private static final char REPLACE_HOOK = '#';
 
     public String nextToken() {
