3b73355f4e9d260f75555387725bcb519f25cf15
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-05-10 16:37:38 +0100

    added new addresses

--- src/main/resources/db/changelog/version-1.0/28.07.2023.part2.insert-address-table-data.sql
+++ src/main/resources/db/changelog/version-1.0/28.07.2023.part2.insert-address-table-data.sql
@@ -19,3 +19,30 @@ VALUES ('f6f6f6f6-f6f6-f6f6-f6f6-f6f6f6f6f6f6', '15 Rue de Rivoli', 'Paris', 'Fr
 
 INSERT INTO address (id, line, city, country, postcode)
 VALUES ('550e8400-e29b-41d4-a716-446655440000', '30 Lombard Street', 'San Francisco', 'USA', '94111');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('11111111-1111-1111-1111-111111111112', 'ul. Tverskaya, dom 10', 'Moscow', 'Russia', '101000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('22222222-2222-2222-2222-222222222223', 'ul. Nevsky pr., dom 20', 'Saint Petersburg', 'Russia', '190000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('33333333-3333-3333-3333-333333333334', 'pr-t Lenina, dom 30', 'Yekaterinburg', 'Russia', '620000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('44444444-4444-4444-4444-444444444445', 'ul. Krasnaya ploshchad, dom 40', 'Nizhny Novgorod', 'Russia', '603000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('55555555-5555-5555-5555-555555555556', 'pr-t Lenina, dom 50', 'Kazan', 'Russia', '420000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('66666666-6666-6666-6666-666666666667', 'ul. Sovetskaya, dom 60', 'Samara', 'Russia', '443000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('77777777-7777-7777-7777-777777777778', 'pr-t Mira, dom 70', 'Omsk', 'Russia', '644000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('88888888-8888-8888-8888-888888888889', 'ul. Lenina, dom 80', 'Chelyabinsk', 'Russia', '454000');
+
+INSERT INTO address (id, line, city, country, postcode)
+VALUES ('99999999-9999-9999-9999-999999999990', 'ul. Kutuzovskaya, dom 90', 'Moscow', 'Russia', '121248');
