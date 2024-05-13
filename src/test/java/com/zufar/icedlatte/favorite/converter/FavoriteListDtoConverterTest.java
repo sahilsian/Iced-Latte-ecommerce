5da8a27e3156e93915c9f76c8836880188f50e29
Ziborova Victoria <vrziborova@yandex.ru>
2024-05-13 16:19:56 +0300

    added tests for converters

---
+++ src/test/java/com/zufar/icedlatte/favorite/converter/FavoriteListDtoConverterTest.java
@@ -0,0 +1,79 @@
+package com.zufar.icedlatte.favorite.converter;
+
+import com.zufar.icedlatte.favorite.dto.FavoriteListDto;
+import com.zufar.icedlatte.favorite.entity.FavoriteItemEntity;
+import com.zufar.icedlatte.favorite.entity.FavoriteListEntity;
+import com.zufar.icedlatte.openapi.dto.ProductInfoDto;
+import com.zufar.icedlatte.product.entity.ProductInfo;
+import com.zufar.icedlatte.user.stub.UserDtoTestStub;
+import org.junit.jupiter.api.BeforeEach;
+import org.mapstruct.factory.Mappers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FavoriteListDtoConverterTest {
+
+    private FavoriteListDtoConverter converter;
+
+    @BeforeEach
+    void setup() {
+        converter = Mappers.getMapper(FavoriteListDtoConverter.class);
+    }
+
+    @Test
+    @DisplayName("Convert FavoriteListEntity to FavoriteListDto")
+    void convertListEntityToDto() {
+
+        ProductInfo productInfo = new ProductInfo();
+        productInfo.setProductId(UUID.randomUUID());
+
+        FavoriteItemEntity favoriteItem = new FavoriteItemEntity();
+        favoriteItem.setProductInfo(productInfo);
+
+        FavoriteListEntity expectedFavoriteListEntity = FavoriteListEntity.builder()
+                .id(UUID.randomUUID())
+                .user(UserDtoTestStub.createUserEntity())
+                .favoriteItems(Set.of(favoriteItem))
+                .updatedAt(OffsetDateTime.now())
+                .build();
+
+        FavoriteListDto actualFavoriteListDto = converter.toDto(expectedFavoriteListEntity);
+
+        assertThat(actualFavoriteListDto.id()).isEqualTo(expectedFavoriteListEntity.getId());
+        assertThat(actualFavoriteListDto.updatedAt()).isEqualTo(expectedFavoriteListEntity.getUpdatedAt());
+        assertThat(actualFavoriteListDto.userId()).isEqualTo(Optional.of(expectedFavoriteListEntity.getUser()).get().getId());
+        // fIXME: how to test favoriteItems?
+//        assertThat(actualFavoriteListDto.favoriteItems()).isEqualTo(expectedFavoriteListEntity.getFavoriteItems());
+
+    }
+
+    @Test
+    @DisplayName("Convert ProductInfo to ProductInfoDto")
+    void convertToProductInfoDto() {
+
+        ProductInfo expectedProductInfo = new ProductInfo(UUID.randomUUID(), "Coffee", "Coffee description",
+                new BigDecimal(100), 1, true,  new BigDecimal(100), 1, "Jacobs", "Seller");
+
+        ProductInfoDto actualProductInfoDto = converter.convertProductInfoDto(expectedProductInfo);
+
+        assertThat(actualProductInfoDto.getId()).isEqualTo(expectedProductInfo.getProductId());
+        assertThat(actualProductInfoDto.getName()).isEqualTo(expectedProductInfo.getName());
+        assertThat(actualProductInfoDto.getDescription()).isEqualTo(expectedProductInfo.getDescription());
+        assertThat(actualProductInfoDto.getPrice()).isEqualTo(expectedProductInfo.getPrice());
+        assertThat(actualProductInfoDto.getQuantity()).isEqualTo(expectedProductInfo.getQuantity());
+        assertThat(actualProductInfoDto.getActive()).isEqualTo(expectedProductInfo.getActive());
+        assertThat(actualProductInfoDto.getAverageRating()).isEqualTo(expectedProductInfo.getAverageRating());
+        assertThat(actualProductInfoDto.getReviewsCount()).isEqualTo(expectedProductInfo.getReviewsCount());
+        assertThat(actualProductInfoDto.getBrandName()).isEqualTo(expectedProductInfo.getBrandName());
+        assertThat(actualProductInfoDto.getSellerName()).isEqualTo(expectedProductInfo.getSellerName());
+    }
+
+}
