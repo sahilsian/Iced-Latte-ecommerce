9692b8a050fb35e78bee4266eea9e0fec0ad17f2
Ziborova Victoria <vrziborova@yandex.ru>
2024-06-10 22:37:27 +0300

    spring ai fix

--- src/main/java/com/zufar/icedlatte/openai/api/ProductReviewOpenAiValidator.java
+++ src/main/java/com/zufar/icedlatte/openai/api/ProductReviewOpenAiValidator.java
@@ -1,20 +1,17 @@
 package com.zufar.icedlatte.openai.api;
 
-import com.zufar.icedlatte.openai.exception.InappropriateContentException;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,40 +20,25 @@ public class ProductReviewOpenAiValidator {
 
     private final OpenAiChatModel chatModel;
 
+    @Value("${spring.ai.openai.prompt}")
+    private String openAiPrompt;
+
     @Autowired
     public ProductReviewOpenAiValidator(OpenAiChatModel chatModel) {
         this.chatModel = chatModel;
     }
 
     @GetMapping("/ai/generate")
-    public Map checkProductReviewContent(@RequestParam("review") String review) {
+    public Boolean isProductReviewContentValid(@RequestParam("review") String review) {
 
-        String promptString = """
-            |You are an assistant who checks texts for appropriateness.
-            |Analyze the following text {review} and determine if it contains political statements,
-            |non-English language, or inappropriate language.
-            |If you find any of these issues, return the phrase 'not appropriate', after return the reason in curly brackets. 
-            |Only the reason should be in curly brackets.
-            |Else return OK
-            """;
-        PromptTemplate promptTemplate = new PromptTemplate(promptString);
+        PromptTemplate promptTemplate = new PromptTemplate(openAiPrompt);
         promptTemplate.add("review", review);
 
         ChatResponse response = chatModel.call(new Prompt(List.of(promptTemplate.createMessage()),
                 OpenAiChatOptions.builder().build()));
 
-        List<Generation> results = response.getResults();
-        Map<String, String> resultMap = new HashMap();
-        for (Generation result: results) {
-            String content = result.getOutput().getContent();
-            if (content.startsWith("not appropriate")) {
-                String reason = extractReason(content);
-                throw new InappropriateContentException(reason);
-            } else {
-                resultMap.put("generation", content);
-            }
-        }
-        return resultMap;
+        return response.getResults().stream()
+                .anyMatch(result -> !result.getOutput().getContent().startsWith("not appropriate"));
     }
 
     private String extractReason(String content) {
