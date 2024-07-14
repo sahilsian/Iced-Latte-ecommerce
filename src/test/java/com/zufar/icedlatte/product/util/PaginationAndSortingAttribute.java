29b9efc12654a301340b7d2fc2f802483c6aa78f
Sahil Sian <sh864246@dal.ca>
2024-07-14 19:06:29 -0300

    Refactored unnecissary abstraction in PaginationAndSortingAttribute by changing from class to interface

--- src/test/java/com/zufar/icedlatte/product/util/PaginationAndSortingAttribute.java
+++ src/test/java/com/zufar/icedlatte/product/util/PaginationAndSortingAttribute.java
@@ -1,9 +1,8 @@
 package com.zufar.icedlatte.product.util;
 
-public class PaginationAndSortingAttribute {
-
-    public static final String PAGE = "page";
-    public static final String SIZE = "size";
-    public static final String SORT_ATTRIBUTE = "sort_attribute";
-    public static final String SORT_DIRECTION = "sort_direction";
+public interface PaginationAndSortingAttribute {
+    String PAGE = "page";
+    String SIZE = "size";
+    String SORT_ATTRIBUTE = "sort_attribute";
+    String SORT_DIRECTION = "sort_direction";
 }
