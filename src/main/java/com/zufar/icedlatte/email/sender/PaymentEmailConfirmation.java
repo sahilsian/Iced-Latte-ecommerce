de5aea1ff88186eaf253be9f6f02446631ec8120
Sahil Sian <sh864246@dal.ca>
2024-07-14 17:20:32 -0300

    Fix: Identified code smell Long Identifier in PaymentEmailConfirmation. Refactored by adding in helper methods

--- src/main/java/com/zufar/icedlatte/email/sender/PaymentEmailConfirmation.java
+++ src/main/java/com/zufar/icedlatte/email/sender/PaymentEmailConfirmation.java
@@ -13,10 +13,8 @@ import java.util.List;
 @Component
 public class PaymentEmailConfirmation extends AbstractEmailSender<EmailConfirmMessage> {
 
-
-    private static final String DEFAULT_SUCCESSFUL_EMAIL_MESSAGE = "You're payment with total amount - %d %s was successfully processed";
-
-    private static final String DEFAULT_EMAIL_SUBJECT = "Payment Confirmation for Your Recent Purchase";
+    private static final String DEFAULT_SUCCESSFUL_MESSAGE_TEMPLATE = "You're payment with total amount - %d %s was successfully processed";
+    private static final String DEFAULT_SUBJECT = "Payment Confirmation for Your Recent Purchase";
 
     @Autowired
     public PaymentEmailConfirmation(JavaMailSender javaMailSender,
@@ -26,10 +24,18 @@ public class PaymentEmailConfirmation extends AbstractEmailSender<EmailConfirmMe
     }
 
     public void send(PaymentIntent paymentIntent) {
-        sendNotification(
-                paymentIntent.getReceiptEmail(),
-                DEFAULT_SUCCESSFUL_EMAIL_MESSAGE.formatted(paymentIntent.getAmount(), paymentIntent.getCurrency()),
-                DEFAULT_EMAIL_SUBJECT
-        );
+        String recipientEmail = paymentIntent.getReceiptEmail();
+        String message = formatSuccessfulMessage(paymentIntent.getAmount(), paymentIntent.getCurrency());
+        String subject = getDefaultSubject();
+
+        sendNotification(recipientEmail, message, subject);
+    }
+
+    private String formatSuccessfulMessage(long amount, String currency) {
+        return DEFAULT_SUCCESSFUL_MESSAGE_TEMPLATE.formatted(amount, currency);
+    }
+
+    private String getDefaultSubject() {
+        return DEFAULT_SUBJECT;
     }
 }
